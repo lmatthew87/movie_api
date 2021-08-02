a4cd381cc2fee3f93ed8ebd3730576cf99806dac
lmatthew87 <81183261+lmatthew87@users.noreply.github.com>
2021-08-02 12:50:49 -0700

    error in something not right

--- auth.js
+++ auth.js
@@ -22,6 +22,7 @@ module.exports = (router) => {
       if (error || !user) {
         return res.status(400).json({
           message: 'Something is not right',
+          error,
           user: user
         });
       }
